9fb388dddb0ebbe940a39d40101253954cbe4338
Scott Roberts <scott.lee.roberts@gmail.com>
2019-02-10 15:05:20 -0500

    Better performance for homerow custom functions
    Pinky shifts and TMUX keys were not playing nice.  These changes seem to
    be the standard QMK solutions to this exact problem

--- config.h
+++ config.h
@@ -22,8 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 // #define USE_I2C
 
-/* Select hand configuration */
 
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+/* Select hand configuration */
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
