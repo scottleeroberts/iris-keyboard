33a8a82d2f5f89c01aab0f22bf2719ec3e15c679
Scott Roberts <scott.lee.roberts@gmail.com>
2021-10-24 15:12:23 -0400

    Make RFB lights work with Rev5

--- config.h
+++ config.h
@@ -21,3 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
--- rules.mk
+++ rules.mk
@@ -4,3 +4,5 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 DEBOUNCE_TYPE = sym_eager_pr
+RGBLIGHT_ENABLE   = yes
+BACKLIGHT_ENABLE = yes
